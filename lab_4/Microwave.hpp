918a633dd970afb381dc6273dfd68a36553c967e
ddvki241 <dmitrivdenus@gmail.com>
2025-12-16 18:54:47 +0200

    Add Microwave class with basic functionality

---
+++ lab_4/Microwave.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <iostream>
+
+enum Mode { MICRO = 0, GRILL = 1 };
+
+class Microwave
+{
+private:
+    bool Power;
+    bool RadiatorOn;
+    bool HeaterOn;
+    bool DoorOpen;
+
+    Mode CurrentMode;
+    int MicroPower;
+    int GrillPower;
+    int Timer;
+
+    void StopAll();
+
+public:
+
+    // === Конструктори ===
+    Microwave();  // за замовчуванням
+    Microwave(const Microwave& other); // копіювання
+    Microwave(bool power, Mode mode, int microPower, int timer, bool door); // ініціалізація
+
+    // === Деструктор ===
+    ~Microwave();
+
+    // === Методи ===
+    void TurnOn();
+    void TurnOff();
+    void SetMode(Mode m);
+    void SetTimer(int seconds);
+    void StartHeating();
+    void OpenDoor();
+    void CloseDoor();
+    double FinishHeating();
+
+    // === Get ===
+    bool GetPower() const;
+    bool GetDoor() const;
+    bool IsRadiatorOn() const;
+    bool IsHeaterOn() const;
+    int GetMicroPower() const;
+    int GetTimer() const;
+    Mode GetMode() const;
+    int GetCurrentPower() const;
+   
+    Microwave& operator=(bool state);      // Вмикання/вимкнення
+    friend int  operator<<=(Microwave& m, int watts);     // Потужність
+    friend int  operator^=(Microwave& m, int seconds);    // Таймер
+};
